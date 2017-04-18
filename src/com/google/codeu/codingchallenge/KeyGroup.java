85d830c247d7b43b0fc43ce8c36221d2473b2a5e
Himani Vyas <himonal@wirelessprv-10-195-47-101.near.illinois.edu>
2017-04-17 22:02:57 -0500

    finished documentation

--- src/com/google/codeu/codingchallenge/KeyGroup.java
+++ src/com/google/codeu/codingchallenge/KeyGroup.java
@@ -3,7 +3,17 @@ package com.google.codeu.codingchallenge;
 import java.security.Key;
 
 /**
- * Created by himonal on 4/17/17.
+ * The purpose of this class is to represent a KeyGroup. A KeyGroup is a series of characters that represent something
+ * meaningful in a JSON-lite object. Each group points to another KeyGroup which should one that is valid to come
+ * after it. For Example an START_OBJ KeyGroup cannot be followed by a NEW_KEY_VALUE_PAIR.
+ * The current KeyGroups are...
+ * START_OBJ = " {" " Series of characters that represent a new object starting
+ * END_OBJ = " "} " Series of characters that represent an object ending
+ * TRANSITION_TO_STRING = " ":" " Series of characters that represent a transition from a key to a string value
+ * TRANSITION_TO_OBJ = " ":{ " Series of characters that represents a transition from a key to an object value
+ * NEW_KEY_VALUE_PAIR = " "," " Series of characters that represents a new key value pair starting
+ * NEW_ITEM_AFTER_OBJ = " "}," " Series of characters that represents a new item after an object
+ * Created by Himani Vyas on 4/17/17.
  */
 public class KeyGroup {
     private String keyGroup;
@@ -16,22 +26,42 @@ public class KeyGroup {
         nextKeyGroup = null;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getKeyGroup() {
         return keyGroup;
     }
 
+    /**
+     *
+     * @return int
+     */
     public int getIndex() {
         return index;
     }
 
+    /**
+     *
+     * @param index int
+     */
     public void setIndex(int index) {
         this.index = index;
     }
 
+    /**
+     *
+     * @return KeyGroup
+     */
     public KeyGroup getNextKeyGroup() {
         return nextKeyGroup;
     }
 
+    /**
+     *
+     * @param next KeyGroup
+     */
     public void setNextKeyGroup(KeyGroup next) {
         nextKeyGroup = next;
     }
--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -17,33 +17,63 @@ package com.google.codeu.codingchallenge;
 import java.lang.reflect.Array;
 import java.util.*;
 
+/**
+ * purpose of this class is to implement the JSON interface. Represents a JSON-lite object that contains strings and
+ * objects. Strings and Objects have their own hashmaps
+ */
 final class MyJSON implements JSON {
 
   private HashMap<String, String> strings = new HashMap<>();
   private HashMap<String, JSON> objects = new HashMap<>();
 
+  /**
+   *
+   * @param name String
+   * @return JSON
+   */
   @Override
   public JSON getObject(String name) {
     return objects.get(name);
   }
 
+  /**
+   *
+   * @param name String
+   * @param value JSON
+   * @return JSON
+   */
   @Override
   public JSON setObject(String name, JSON value) {
     objects.put(name, value);
     return this;
   }
 
+  /**
+   *
+   * @param name String
+   * @return String
+   */
   @Override
   public String getString(String name) {
     return strings.get(name);
   }
 
+  /**
+   *
+   * @param name String
+   * @param value String
+   * @return JSON
+   */
   @Override
   public JSON setString(String name, String value) {
     strings.put(name, value);
     return this;
   }
 
+  /**
+   *
+   * @param names Collection<String>
+   */
   @Override
   public void getObjects(Collection<String> names) {
     Iterator it = objects.entrySet().iterator();
@@ -53,6 +83,10 @@ final class MyJSON implements JSON {
     }
   }
 
+  /**
+   *
+   * @param names Collection<String>
+   */
   @Override
   public void getStrings(Collection<String> names) {
     Iterator it = strings.entrySet().iterator();
